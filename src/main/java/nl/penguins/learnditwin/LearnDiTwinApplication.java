211ee98c6e7a393b36785e9e7fd3897309db93a1
NielsStudent <niels.hoogeboom@student.hu.nl>
2023-10-10 20:46:14 +0200

    Eerste commit

---
+++ src/main/java/nl/penguins/learnditwin/LearnDiTwinApplication.java
@@ -0,0 +1,13 @@
+package nl.penguins.learnditwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDiTwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnDiTwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/penguins/learnditwin/LearnDiTwinApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.penguins.learnditwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDiTwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
