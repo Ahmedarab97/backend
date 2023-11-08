c9b828074b8c4778778b6b26d36101c3b0308858
evavisser01 <eva.visser@student.hu.nl>
2023-11-08 16:56:17 +0100

    data in resources gezet en data wordt opgeslagen in database

--- src/main/java/nl/penguins/learnditwin/plaats/data/PlaatsRepository.java
+++ src/main/java/nl/penguins/learnditwin/plaats/data/PlaatsRepository.java
@@ -2,6 +2,8 @@ package nl.penguins.learnditwin.plaats.data;
 
 import nl.penguins.learnditwin.plaats.domain.Wijk;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaatsRepository extends MongoRepository<Wijk, String> {
 }
